ba2492b339db29c67ae776e117962f9b7f0a0bc6
dj-pmfst <djuric@pmfst.hr>
2026-03-03 21:24:26 +0100

    add theme switch to layout and create countdown hook

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import { ReactNode } from 'react'
+import { useTheme } from '../../hooks/useTheme'
 import styles from './Layout.module.css'
 
 interface Props {
@@ -7,6 +8,12 @@ interface Props {
 }
 
 export function Layout({ children }: Props) {
+  const { theme, dispatch } = useTheme()
+
+  const toggleTheme = () => {
+    dispatch({ type: theme === 'dark' ? 'SET_LIGHT' : 'SET_DARK' })
+  }
+
   return (
     <div className={styles.container}>
       <header>
@@ -14,6 +21,9 @@ export function Layout({ children }: Props) {
           <NavLink to="/">Home</NavLink>
           <NavLink to="/launches">Launches</NavLink>
           <NavLink to="/ships">Ships</NavLink>
+          <button onClick={toggleTheme}>
+            {theme === 'dark' ? 'Light' : 'Dark'}
+          </button>
         </nav>
       </header>
       <main className={styles.main}>
