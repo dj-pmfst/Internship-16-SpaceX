6d63df113625bd6d9b311353c67bed81571a12b0
dj-pmfst <djuric@pmfst.hr>
2026-03-03 13:49:52 +0100

    wrap main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
