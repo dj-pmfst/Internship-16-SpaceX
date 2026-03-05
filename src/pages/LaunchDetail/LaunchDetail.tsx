a89cef0650c6988153884f610a5467f36ce48007
dj-pmfst <djuric@pmfst.hr>
2026-03-05 20:03:19 +0100

    implement hook in launch details

--- src/pages/LaunchDetail/LaunchDetail.tsx
+++ src/pages/LaunchDetail/LaunchDetail.tsx
@@ -1,24 +1,14 @@
 import { useParams, useNavigate } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import { getLaunchById, getRocketById } from '../../api'
+import useLaunchDetail from '../../hooks/useLaunchDetails'
 import styles from './LaunchDetail.module.css'
 
 export default function LaunchDetail() {
   const { id } = useParams<{ id: string }>()
   const navigate = useNavigate()
 
-  const { data: launch, isLoading: launchLoading, isError } = useQuery({
-    queryKey: ['launch', id],
-    queryFn: () => getLaunchById(id!),
-  })
+  const { launch, rocket, isLoading, isError } = useLaunchDetail(id!)
 
-  const { data: rocket, isLoading: rocketLoading } = useQuery({
-    queryKey: ['rocket', launch?.rocket],
-    queryFn: () => getRocketById(launch!.rocket),
-    enabled: !!launch?.rocket, 
-  })
-
-  if (launchLoading || rocketLoading) 
+  if (isLoading) 
     return <div className={styles.spinner} />
   if (isError) 
     return <p>Failed to load launch details.</p>
