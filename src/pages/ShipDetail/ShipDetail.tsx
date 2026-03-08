aba111150c570b36237b8b3685d78165acaf15d8
dj-pmfst <djuric@pmfst.hr>
2026-03-08 11:10:10 +0100

    start ship detail page

--- src/pages/ShipDetail/ShipDetail.tsx
+++ src/pages/ShipDetail/ShipDetail.tsx
@@ -1,9 +1,19 @@
 import { useParams, useNavigate } from "react-router-dom";
 import styles from './ShipDetail.module.css'
+import useShipDetail from "../../hooks/useShipDetail";
 
 export default function ShipDetail(){
     const { id } = useParams<{ id: string }>()
     const navigate = useNavigate()
+
+    const { ship, isLoading, isError } = useShipDetail(id!)
+
+    if (isLoading) 
+    return <div className={styles.spinner} />
+    if (isError) 
+        return <p>Failed to load ship details.</p>
+    if (!ship) 
+        return null
 
     return (
         <div className={styles.container}>
